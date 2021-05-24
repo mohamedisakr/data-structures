f0e1176b0877d1353dbd8770504dbbec7a6a243c
mohamedisakr <mohamedisakr@gmail.com>
2021-05-24 21:52:45 +0200

    implemting mystring data type

---
+++ adt/mystring.js
@@ -0,0 +1,43 @@
+/** MyString represents an immutable sequence of characters. */
+class MyString {
+  constructor() {
+    this.chars = [];
+  }
+  //////////////////// Example of a creator operation ///////////////
+  /**
+   * @param b a boolean value
+   * @return string representation of b, either "true" or "false"
+   */
+  static valueOf(b) {}
+
+  //////////////////// Examples of observer operations ///////////////
+  /**
+   * @return number of characters in this string
+   */
+  length() {
+    return this.chars.length;
+  }
+
+  /**
+   * @param i character position (requires 0 <= i < string length)
+   * @return character at position i
+   */
+  charAt(i) {
+    return this.chars[i];
+  }
+
+  //////////////////// Example of a producer operation ///////////////
+  /**
+   * Get the substring between start (inclusive) and end (exclusive).
+   * @param start starting index
+   * @param end ending index.  Requires 0 <= start <= end <= string length.
+   * @return string consisting of charAt(start)charAt(end-1)
+   */
+  substring(start, end) {
+    return this.chars.slice(start, end);
+  }
+
+  /////// no mutator operations (why not?) -> because it is immutable
+}
+
+export { MyString };
