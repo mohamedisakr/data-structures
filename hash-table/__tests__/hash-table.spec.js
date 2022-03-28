58ea5ac671a37cb7a6538ae3de5806b2e571d7cb
mohamedisakr <mohamedisakr@gmail.com>
2022-03-28 13:57:46 +0200

    adding hashtable put * get test cases

--- hash-table/__tests__/hash-table.spec.js
+++ hash-table/__tests__/hash-table.spec.js
@@ -10,4 +10,33 @@ describe("hash table", () => {
     const hashTable = new HashTable();
     expect(hashTable.isEmpty()).toBeTruthy();
   });
+
+  it("add new item, should increase length", () => {
+    const hashTable = new HashTable();
+    const key = "johndoe";
+    const value = 123456789;
+    hashTable.put(key, value);
+    expect(hashTable.isEmpty()).toBeFalsy();
+  });
+
+  it("get (null) key, should return undefind", () => {
+    const hashTable = new HashTable();
+    const result = hashTable.get(null);
+    expect(result).toBeUndefined();
+  });
+
+  it("get non existing key, should return undefind", () => {
+    const hashTable = new HashTable();
+    const result = hashTable.get("johndoe");
+    expect(result).toBeUndefined();
+  });
+
+  it("get existing key, should return its value", () => {
+    const hashTable = new HashTable();
+    const key = "johndoe";
+    const value = 123456789;
+    hashTable.put(key, value);
+    const result = hashTable.get(key);
+    expect(result).toBe(value);
+  });
 });
